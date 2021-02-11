1d499cce22e687c8d200a2f2b3d470393e7b1e3c
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2021-02-11 13:49:55 +0530

    Update login.dart
    - Fixed bugs in responsiveness

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,18 +9,24 @@ import 'package:foodieadmin/widgets/textBox.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Container(
-          height: MediaQuery.of(context).size.height,
-          color: Colors.black,
-          width: double.maxFinite,
-          child:  SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child:SizedBox(
-          width: double.maxFinite,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.black,
+        width: double.maxFinite,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: SizedBox(
+            width: double.maxFinite,
+            height: size.width > 530
+                ? MediaQuery.of(context).size.width
+                : size.height,
+            child: Column(
+                mainAxisAlignment: size.height < 530
+                    ? MainAxisAlignment.start
+                    : MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Image.asset(
@@ -39,16 +45,13 @@ class Login extends StatelessWidget {
                             PasswordBox(),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                CustomTextButton(),
-                                ColorButton()
-                              ],
+                              children: [CustomTextButton(), ColorButton()],
                             )
                           ]),
                     ),
                   ),
                 ]),
-        ),
+          ),
         ),
       ),
     );
