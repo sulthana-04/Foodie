82cc819d696eb5af2e3e613c825bc7ce5b5b130f
Anumodh <51357458+Anumodhkj@users.noreply.github.com>
2021-02-11 12:25:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(home:Foodie()));
+}
+
+class Foodie extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
