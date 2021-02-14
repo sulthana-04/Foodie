76e95232fd90779d6a681a5f64ad03697f36e352
Anumodh <51357458+Anumodhkj@users.noreply.github.com>
2021-02-14 17:27:21 +0530

    Dashboard basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:foodieadmin/goldPages/orders.dart';
 import 'package:foodieadmin/goldPages/splashScreen.dart';
 
 void main() {
     WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Orders()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Foodie()));
 }
